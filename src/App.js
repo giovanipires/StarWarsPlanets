9dd67ab28a755f720834699577f12f82753cbe1c
Giovani Pires <giovani.pires@gmail.com>
2019-06-26 10:14:38 -0300

    second commit;

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Card from './components/Card';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      planets: [],
+    }
+
+    this.getPlanets = this.getPlanets.bind(this)
+  }
+
+  getRandomPlanet(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  //HOJE ESTA LIMITADO AO MIN E MAX APRESENTADO
+  //COLOCAR TODOS OS PLANETAS EM LOCALSTORAGE?
+  //FAZER CONSULTA POR TODOS OS PLANETAS? E A LIMITAÇÃO E PAGINAÇÃO?
+
+  getPlanets() {
+    let random = this.getRandomPlanet(1, 61);
+    console.log(random);
+    
+    return axios.get("https://swapi.co/api/planets/" + random + "/")
+    .then((response) => {
+      // console.log(response.data);
+      this.setState( { planets: response.data })
+    });
+  }
+
+  componentDidMount(){
+    this.getPlanets()
+  }
+
+  render() {
+    const {planets} = this.state;
+    return(
+      <div className="App">
+        <Card planets = { planets } />
+        <br></br>
+        <button onClick={this.getPlanets} className="btn btn-warning"><strong>Next</strong></button>
+      </div>
+    );
+  }
 }
 
 export default App;
